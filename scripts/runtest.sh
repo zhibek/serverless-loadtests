e25da7118ed2b27bf4860f8e08b985a8704a0b52
John Levermore <john.levermore@zhibek.com>
2019-01-17 09:16:51 +0000

    Update bash scripts to make them more robust

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-slsart invoke
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+if [ -z "$1" ]; then
+    echo "Please ensure a 'scenario' script is specified as an argument - runtest.sh <scenario>"
+    exit 1
+fi
+SCENARIO_SCRIPT="/src/scenarios/$1/script.yml"
+if [ ! -f $SCENARIO_SCRIPT ]; then
+    echo "Scenario script not found at $SCENARIO_SCRIPT - please check and try again"
+    exit 1
+fi
+echo "Using the following scenario script - $SCENARIO_SCRIPT"
+
+slsart invoke --path $SCENARIO_SCRIPT
