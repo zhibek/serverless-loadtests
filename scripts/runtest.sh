1b0f4ea822e8a7348da3df2481df7c8bb7086476
John Levermore <john.levermore@zhibek.com>
2019-01-17 14:42:50 +0000

    Modify runtest.sh script to allow both directory and script file to be specified as arguments

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -4,14 +4,22 @@
 set -e
 
 if [ -z "$1" ]; then
-    echo "Please ensure a 'scenario' script is specified as an argument - runtest.sh <scenario>"
+    echo "Please ensure a 'scenario' group is specified as an argument - runtest.sh <scenario>"
     exit 1
 fi
-SCENARIO_SCRIPT="/src/scenarios/$1/script.yml"
-if [ ! -f $SCENARIO_SCRIPT ]; then
-    echo "Scenario script not found at $SCENARIO_SCRIPT - please check and try again"
-    exit 1
-fi
-echo "Using the following scenario script - $SCENARIO_SCRIPT"
+SCENARIO_GROUP="$1"
 
-slsart invoke --path $SCENARIO_SCRIPT
+if [ -z "$2" ]; then
+    SCENARIO_SCRIPT="script"
+else
+    SCENARIO_SCRIPT="$2"
+fi
+
+SCENARIO_PATH="/src/scenarios/$SCENARIO_GROUP/$SCENARIO_SCRIPT.yml"
+if [ ! -f $SCENARIO_PATH ]; then
+    echo "Scenario path not found at $SCENARIO_PATH - please check and try again"
+    exit 1
+fi
+echo "Using the following scenario path - $SCENARIO_PATH"
+
+slsart invoke --path $SCENARIO_PATH
